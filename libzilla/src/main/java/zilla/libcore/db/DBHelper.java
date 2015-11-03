6856e6a024260d4b0350e84a45f94dbf225d2b80
zilla <zillachan@gmail.com>
2015-11-03 11:15:48 +0800

    <imp zilla>Add some db annotation,not implement

--- libzilla/src/main/java/zilla/libcore/db/DBHelper.java
+++ libzilla/src/main/java/zilla/libcore/db/DBHelper.java
@@ -60,6 +60,15 @@ public final class DBHelper extends SQLiteOpenHelper {
     }
 
     @Override
+    public void onOpen(SQLiteDatabase db) {
+        super.onOpen(db);
+        if(!db.isReadOnly()) {
+            // Enable foreign key constraints
+            db.execSQL("PRAGMA foreign_keys=ON;");
+        }
+    }
+
+    @Override
     public void onCreate(SQLiteDatabase db) {
         if (dbUpgradeListener != null) {
             dbUpgradeListener.onCreate(db);
