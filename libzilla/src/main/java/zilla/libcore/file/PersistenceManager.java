1384a9b3ba4e160cd37e99c6f9a654c086c0b9f4
zilla <zilla.chen@softtek.com>
2016-05-27 14:46:46 +0800

    <imp zilla>add PersistenceManager to manage obj serialize

---
+++ libzilla/src/main/java/zilla/libcore/file/PersistenceManager.java
@@ -0,0 +1,60 @@
+/*
+Copyright 2015 Zilla Chen
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+ */
+package zilla.libcore.file;
+
+import java.io.Serializable;
+import java.lang.ref.WeakReference;
+import java.util.HashMap;
+
+/**
+ * 对象持久化工具类<br>
+ * 处理对象的持久化和读取
+ *
+ * @author ze.chen
+ * @version 1.0
+ */
+public class PersistenceManager {
+
+    private static HashMap<String, WeakReference<Serializable>> cache = new HashMap<>();
+
+    /**
+     * 读取对象
+     *
+     * @param name
+     * @return
+     */
+    public static Serializable readObj(String name) {
+        Object obj = cache.get(name).get();
+        if (obj == null) {
+            obj = FileHelper.readObj(FileHelper.PATH_FILES + name + ".obj");
+            if (obj == null) {
+                return null;
+            }
+        }
+        return (Serializable) obj;
+    }
+
+    /**
+     * 保存对象
+     *
+     * @param obj
+     * @return
+     */
+    public static boolean saveObj(Serializable obj) {
+        cache.put(obj.getClass().getSimpleName(), new WeakReference<Serializable>(obj));
+        return FileHelper.saveObj(obj, FileHelper.PATH_FILES + obj.getClass().getSimpleName() + ".obj");
+    }
+}
